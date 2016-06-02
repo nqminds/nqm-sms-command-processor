ff7901d7b7d17c07b3d1eeaa9219165147a40875
toby ealden <toby.ealden@gmail.com>
2016-06-02 11:18:52 +0100

    add dongle connect support

--- index.js
+++ index.js
@@ -14,25 +14,31 @@
     log("message received: %j", msg);
     if (msg.body.indexOf("#") === 0) {
       // Strip leading command prefix "#"
-      var command = msg.body.substr(1);
+      var command = msg.body.substr(1).trim();
       // Check this is a shell command.
       if (command.indexOf("!") === 0) {
         handleShellCommand(msg);
       } else {
-        switch (command.trim()) {
+        // Strip off params.
+        command = command.split(" ")[0];
+        switch (command) {
           case "dongle-reboot":
-            // Careful - this command appears to sometimes break the dongle interface.
-            // The dongle appears to prefer the device is rebooted with the dongle in place,
-            // rather than the dongle rebooted (or hot-plugged) while the device is powered on.
-            monitor.sendResponse(msg.from, "rebooting dongle", msg.id);
-            monitor.rebootDevice();
+            dongleReboot(msg);
+            break;
+          case "dongle-get":
+            dongleGet(msg);
+            break;
+          case "dongle-connect":
+            dongleConnect(msg);
             break;
           case "update":
-            // Pull the latest sms command processor (i.e. this process)
             pullLatest(msg);
             break;
           case "status":
             getInterliNQStatus(msg);
+            break;
+          case "pending":
+            getPendingFiles(msg);
             break;
           default:
             log("unknown non-shell command => ignoring %s", command);
@@ -47,7 +53,48 @@
 
   monitor.start(10000);
 
+  var dongleReboot = function(msg) {
+    // Careful - this command appears to sometimes break the dongle interface.
+    // The dongle appears to prefer the device is rebooted with the dongle in place,
+    // rather than the dongle rebooted (or hot-plugged) while the device is powered on.
+    monitor.sendResponse(msg.from, "rebooting dongle", msg.id);
+    monitor.rebootDevice();    
+  };
+  
+  var dongleGet = function(msg) {
+    var params = msg.body.split(" ");
+    if (params.length > 1) {
+      monitor.getParam(params[1], function(err, param) {
+        if (err) {
+          monitor.sendResponse(msg.from, "failed to get param: " + err.message, msg.id);
+        } else {
+          log("dongleGet: [%j]",param);
+          monitor.sendResponse(msg.from, JSON.stringify(param), msg.id); 
+        }                  
+      });               
+    } else {
+      monitor.sendResponse(msg.from, "invalid param request: " + command, msg.id);
+    }                
+  };
+  
+  var dongleConnect = function(msg) {
+    monitor.connect(function(err, result) {
+      if (err) {
+        monitor.sendResponse(msg.from, "failed to connect: " + err.message, msg.id);
+      } else {
+        monitor.sendResponse(msg.from, "dongleConnect: " + result, msg.id);
+      }
+    });
+  };
+  
+  var getPendingFiles = function(msg) {
+    log("get count of files pending transmission");
+    executeShellCommand("ls /interliNQ/ucontrol-client/monitor/transmit/*.log | wc -l", msg, "pending transmit");
+  };
+  
   var pullLatest = function(msg) {
+    // Pull the latest sms command processor (i.e. this process)
+    // n.b. will require a reboot to be effective.
     log("pulling latest code from git");
     executeShellCommand("git fetch -v origin master:refs/remotes/origin/master", msg);
   };
@@ -68,14 +115,28 @@
     var shellCommand = msg.body.substr(2);
     executeShellCommand(shellCommand, msg);
   };  
+  
+  var joinText = function(a,b,join) {
+    var joined = "";
+    if (a) {
+      joined += a;
+    }
+    if (b) {
+      if (joined.length > 0) {
+        joined += join;
+      }
+      joined += b;
+    }
+    return joined;
+  };
 
-  var executeShellCommand = function(shellCommand, msg) {
+  var executeShellCommand = function(shellCommand, msg, prefixResponse) {
     log("executing shell command '%s'", shellCommand);
     shell.exec(shellCommand, {silent:true}, function(code, output, err) {
       log("shell exec result code %d [%j], err: [%j]", code, output, err);
       // Send SMS response.
-      var responseText = [output, err].join(" - ");
-      monitor.sendResponse(msg.from, responseText, msg.id);
+      var responseText = joinText(output, err, " - ");
+      monitor.sendResponse(msg.from, joinText(prefixResponse,responseText,": "), msg.id);
     });    
   };  
 }());
